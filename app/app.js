76aa2aee394419dd60143abd42a74ef91d853fc6
Chris Lyth <cjlyth@gmail.com>
2016-12-11 02:28:02 -0600

    Fixed a state issue that prevented the active tab to be highlighted

--- app/app.js
+++ app/app.js
@@ -1,54 +1,60 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', ['ngRoute','myApp.version','ui.router'])
-.config(function ($stateProvider, $urlRouterProvider) {
+angular.module('myApp', ['ngRoute', 'myApp.version', 'ui.router'])
+  .run([
+    "$rootScope", "$state", "$stateParams", function ($rootScope, $state, $stateParams) {
+      $rootScope.$state = $state;
+      // return $rootScope.$stateParams = $stateParams;
+    }
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
 
- $urlRouterProvider.otherwise('/home');
-  $stateProvider
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
       .state('home', {
-          url: '/home',
-          templateUrl: 'main/templates/applicationStatus.html',
-          controller:'ApplicationStatusCtrl as ctrl'
+        url: '/home',
+        templateUrl: 'main/templates/applicationStatus.html',
+        controller: 'ApplicationStatusCtrl as ctrl'
       })
       .state('applicationInformation', {
-          url: '/applicationInformation',
-          templateUrl: 'main/templates/applicationInformation.html',
-          controller:'ApplicationInformationCtrl as ctrl'
+        url: '/applicationInformation',
+        templateUrl: 'main/templates/applicationInformation.html',
+        controller: 'ApplicationInformationCtrl as ctrl'
       })
       .state('assistanceRequest', {
-          url: '/assistanceRequest',
-          templateUrl: 'main/templates/assistanceRequest.html',
-          controller:'AssistanceRequestCtrl as ctrl'
+        url: '/assistanceRequest',
+        templateUrl: 'main/templates/assistanceRequest.html',
+        controller: 'AssistanceRequestCtrl as ctrl'
       })
-      .state('eligablePersonnel', {
-          url: '/eligablePersonnel',
-          templateUrl: 'main/templates/eligablePersonnel.html',
-          controller:'EligablePersonnelCtrl as ctrl'
+      .state('eligiblePersonnel', {
+        url: '/eligiblePersonnel',
+        templateUrl: 'main/templates/eligablePersonnel.html',
+        controller: 'EligablePersonnelCtrl as ctrl'
       })
       .state('incidentInfo', {
-          url: '/incidentInfo',
-          templateUrl: 'main/templates/incidentInfo.html',
-          controller:'IncidentInfoCtrl as ctrl'
+        url: '/incidentInfo',
+        templateUrl: 'main/templates/incidentInfo.html',
+        controller: 'IncidentInfoCtrl as ctrl'
       })
       .state('other', {
-          url: '/other',
-          templateUrl: 'main/templates/other.html',
-          controller:'OtherCtrl as ctrl'
+        url: '/other',
+        templateUrl: 'main/templates/other.html',
+        controller: 'OtherCtrl as ctrl'
       })
       .state('review-submission', {
-          url: '/review-submission',
-          templateUrl: 'main/templates/review-submission.html',
-          controller:'ReviewSubmissionCtrl as ctrl'
+        url: '/review-submission',
+        templateUrl: 'main/templates/review-submission.html',
+        controller: 'ReviewSubmissionCtrl as ctrl'
       })
       .state('login', {
-          url: '/login',
-          templateUrl: 'main/templates/login.html',
-          controller:'LoginCtrl as ctrl'
+        url: '/login',
+        templateUrl: 'main/templates/login.html',
+        controller: 'LoginCtrl as ctrl'
       })
 
       .state('about', {
         url: '/about',
         templateUrl: 'main/templates/partial-about.html'
       });
-});
+  });
