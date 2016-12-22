1990d6c1e1cacf5f8316b5b949c160568e0b335a
AutoZone <autozone@AutoZones-MacBook-Pro-2.local>
2016-12-22 10:42:38 -0600

    added a model to the application Info controller and http post logic to app info controller

--- app/main/controllers/applicationInformation-ctrl.js
+++ app/main/controllers/applicationInformation-ctrl.js
@@ -1,8 +1,31 @@
 'use strict';
 angular.module('myApp')
-.controller('ApplicationInformationCtrl', function ($scope, $state) {
+.controller('ApplicationInformationCtrl', function ($scope, $state, $http) {
 
+$scope.saveAppInfo = function (passedIn)
+{
+var url = "http://skyline.autozone.com:5555/api/request/assistance";
+var config = {
+                headers : {
+                    'Content-Type': 'application/json;'
+                }
+            };
+var data = {
+                user_id: passedIn.employeeID,
+                name: passedIn.firstName,
+                data: passedIn.address1
+            };
+            console.log(data);
 
+$http.post(url, data)
+           .success(function (data, status, headers, config) {
+             console.log("success");
+             $state.go('eligiblePersonnel',[]);
+           })
+           .error(function (data, status, header, config) {
+             console.log("error");
+           });
+};
 
 
 });
