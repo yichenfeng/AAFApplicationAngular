a93b31c7cee1b9f4240bfe067c4930927778173f
Ryan Turner <ryan.e.t@gmail.com>
2017-01-19 14:52:43 -0600

    Initial work on the data service

---
+++ app/main/services/data-service.js
@@ -0,0 +1,70 @@
+'use strict';
+angular.module('myApp')
+.factory('DataService', function ($http) {
+  var dataService = {};
+  // factory function body that constructs shinyNewServiceInstance
+  dataService.application = {};
+
+  dataService.getApplicationById = function(id) {
+    if(!id) {
+      console.log("Unable to get application because there is no ID");
+      return false;
+    }
+    return $http({
+      url: '/api/request/assistance/' + id,
+      method: 'GET',
+      headers: {
+        'OpenAMHeaderID': '10705332'
+      }
+    }).then(function successCallback(response) {
+      if (response.data) {
+        dataService.application = response.data.result;
+        return response.data.result;
+      }
+      return false;
+    });
+  };
+
+  dataService.getApplicationsForEmployee = function(employeeId) {
+    //todo
+  };
+
+  dataService.createApplication = function(requestContent) {
+    return $http({
+      url: '/api/request/assistance',
+      method: 'POST',
+      headers: {
+        'OpenAMHeaderID': '10705332' //TODO: remove this, it wont be needed in prod
+      },
+      data: requestContent
+    }).then(function callback(response) {
+      if (response.data && response.data.status == "success") {
+        return dataService.getApplicationById(response.data.result);
+      }
+      return false;
+    });
+  };
+
+  dataService.updateApplication = function(application) {
+    if(!application._id) {
+      console.log("Unable to update application because there is no ID");
+      return false;
+    }
+    return $http({
+      url: '/api/request/assistance/' + application._id,
+      method: 'POST',
+      headers: {
+        'OpenAMHeaderID': '10705332'
+      },
+      data: application
+    }).then(function callback(response) {
+      if (response.data && response.data.status == "success") {
+        dataService.application = application;
+        return application;
+      }
+      return false;
+    });
+  };
+
+  return dataService;
+});
