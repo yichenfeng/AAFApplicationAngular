f3c71f9cf03e58018c5e9a4e87ea4d74d25280b9
PHampy <philiphampy@autozone.com>
2017-01-19 15:51:43 -0600

    Added code for UI work bobby created

--- app/main/controllers/assistanceRequest-ctrl.js
+++ app/main/controllers/assistanceRequest-ctrl.js
@@ -1,8 +1,30 @@
 'use strict';
 angular.module('myApp')
-.controller('AssistanceRequestCtrl', function ($scope, $state) {
+.controller('AssistanceRequestCtrl', function ($scope, $state, ModalService) {
 
 
+  $scope.saveRequestInfo = function (info)
+  {
 
+  {
+    var amount = info.amount;
+      var shelter = info.shelter;
+      var funeral = info.funeral;
+      var utilities = info.utilities;
+      var fire = info.fire;
+      var naturalDisaster = info.naturalDisaster;
+      var other = info.other;
+
+console.log(info);
+
+if (fire || funeral == true){
+console.log("true");
+}
+else {
+  console.log("not true");
+}
+
+  };
+    };
 
 });
