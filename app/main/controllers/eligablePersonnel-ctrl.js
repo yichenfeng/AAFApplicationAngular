bb3498d12473865f8be1a36bbe469d6c156b5064
katherinetucker <ktucker1024@gmail.com>
2017-01-23 14:21:17 -0600

    #18: added service integration and updated ui

--- app/main/controllers/eligablePersonnel-ctrl.js
+++ app/main/controllers/eligablePersonnel-ctrl.js
@@ -2,72 +2,49 @@
 angular.module('myApp')
 .controller('EligablePersonnelCtrl', function ($scope, $state, $rootScope) {
 
+  var createPersonnel = function () {
+    var persons = [];
+    var row = 0;
+    for (var i=0; i<5; i++) {
+      row++;
+      var person = {
+        'row' : row,
+        'fName' : '',
+        'initial' : '',
+        'lName' : '',
+        'age' : '',
+        'relationship' : ''
+      };
+      persons.push(person);
+    }
+    return persons;
+  };
+
+  if($rootScope.application) {
+    if ($rootScope.application.request_content.updatedData.eligible_personnel) {
+       $scope.persons = $rootScope.application.request_content.updatedData.eligible_personnel;
+    }
+  } else {
+    $scope.persons = createPersonnel();
+  }
+
   $scope.saveForLater = function () {
     $state.go('home');
   };
 
-  $scope.saveInfo = function() {
-    if ($rootScope.application) { //application created
-      var person = {};
-      $scope.persons = [];
-      if ($scope.fName1) {
-        person = {
-          'fName' : $scope.fName1,
-          'initial' : $scope.initial1,
-          'lName' : $scope.lName1,
-          'age' : $scope.age1,
-          'relationship' : $scope.relationship1,
+  $scope.$watch('persons', function (newValue) {
+    if ($rootScope.application) {
+      if ($rootScope.application.request_content.updatedData) {
+        $rootScope.application.request_content.updatedData.eligible_personnel = newValue;
+      } else {
+        $rootScope.application.request_content.updatedData = {
+            'eligible_personnel' : newValue
         };
-        $scope.persons.push(person);
-      }
-      if ($scope.fName2) {
-        person = {
-          'fName' : $scope.fName2,
-          'initial' : $scope.initial2,
-          'lName' : $scope.lName2,
-          'age' : $scope.age2,
-          'relationship' : $scope.relationship2,
-        };
-        $scope.persons.push(person);
-      }
-      if ($scope.fName3) {
-        person = {
-          'fName' : $scope.fName3,
-          'initial' : $scope.initial3,
-          'lName' : $scope.lName3,
-          'age' : $scope.age3,
-          'relationship' : $scope.relationship3,
-        };
-        $scope.persons.push(person);
-      }
-      if ($scope.fName4) {
-        person = {
-          'fName' : $scope.fName4,
-          'initial' : $scope.initial4,
-          'lName' : $scope.lName4,
-          'age' : $scope.age4,
-          'relationship' : $scope.relationship4,
-        };
-        $scope.persons.push(person);
-      }
-      if ($scope.fName5) {
-        person = {
-          'fName' : $scope.fName5,
-          'initial' : $scope.initial5,
-          'lName' : $scope.lName5,
-          'age' : $scope.age5,
-          'relationship' : $scope.relationship5,
-        };
-        $scope.persons.push(person);
-      }
-      if ($scope.persons.length > 0) {
-        $rootScope.application.request_content.updatedData = $scope.persons;
       }
     }
-  };
+  }, true);
 
   $scope.next = function () {
-    $scope.saveInfo();
     $state.go('incidentInfo');
   };
 
