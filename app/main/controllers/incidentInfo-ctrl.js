1aa35c45ab2fe8d936c51530c7d71e73a09b3ec9
katherinetucker <ktucker1024@gmail.com>
2017-01-23 11:04:02 -0600

    #19 made changes based on review comments

--- app/main/controllers/incidentInfo-ctrl.js
+++ app/main/controllers/incidentInfo-ctrl.js
@@ -3,115 +3,34 @@ angular.module('myApp').controller('IncidentInfoCtrl', function ($scope, $http, 
 
   $scope.max = 1000;
   $scope.count = 0;
-
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-
-  $scope.clear = function() {
-    $scope.dt = null;
-  };
+  $scope.isOpen = false;
+  $scope.showErrorMessage = false;
 
   $scope.inlineOptions = {
-    customClass: getDayClass,
-    minDate: new Date(),
     showWeeks: true
   };
-
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    maxDate: new Date(2020, 5, 22),
-    minDate: new Date(),
-    startingDay: 1
-  };
-
-  $scope.toggleMin = function() {
-    $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
-    $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
-  };
-
-  $scope.toggleMin();
-
-  $scope.open1 = function() {
-    $scope.popup1.opened = true;
-  };
-
-  $scope.setDate = function(year, month, day) {
-    $scope.dt = new Date(year, month, day);
-  };
-
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd/MM/yyyy', 'MM/dd/yyyy', 'shortDate'];
-  $scope.format = $scope.formats[3];
-  $scope.altInputFormats = ['M!/d!/yyyy'];
-
-  $scope.popup1 = {
-    opened: false
-  };
-
-  var tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  var afterTomorrow = new Date();
-  afterTomorrow.setDate(tomorrow.getDate() + 1);
-  $scope.events = [
-    {
-      date: tomorrow,
-      status: 'full'
-    },
-    {
-      date: afterTomorrow,
-      status: 'partially'
-    }
-  ];
-
-  function getDayClass(data) {
-    var date = data.date,
-        mode = data.mode;
-    if (mode === 'day') {
-      var dayToCheck = new Date(date).setHours(0,0,0,0);
-      for (var i = 0; i < $scope.events.length; i++) {
-        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-        if (dayToCheck === currentDay) {
-          return $scope.events[i].status;
-        }
-      }
-    }
-    return '';
-  }
 
   $scope.saveForLater = function () {
     $state.go('home');
   };
 
-  $scope.saveInfo = function() {
-    if ($scope.incidentInfoForm.$valid) {
-      $scope.dt = new Date(document.getElementById('date').value);
-      $scope.dt = $filter('date')($scope.dt, 'MM/dd/yyyy');
-
-      if ($rootScope.application) { //application created
-        $rootScope.application.request_content.updatedData = {
-           'incident_date' : $scope.dt,
-           'incident_description' : $scope.description
-        };
-      }
+  $scope.dateChanged = function () {
+    $scope.showErrorMessage = false;
+    if ($rootScope.application) {
+      var date = new Date(document.getElementById('date').value);
+      $rootScope.application.request_content.updatedData = {
+        'incident_date' : $filter('date')(date, 'MM/dd/yyyy')
+      };
     }
   };
 
-  $scope.next = function () {
+  $scope.next = function (event) {
     if ($scope.incidentInfoForm.$valid) {
-      $scope.saveInfo();
       $state.go('assistanceRequest');
+    } else {
+      $scope.showErrorMessage = true;
+      event.preventDefault();
     }
   };
 
-}).directive('limitWords', [
-  function() {
-    return function(scope, element, attrs) {
-      element.bind('keyup', function() {
-        scope.count = this.value.split(' ').length;
-        if (scope.count > scope.max) {
-          this.maxLength = this.value.length;
-        }
-      });
-    };
-  }
-]);
+});
