7837070b6c25536f737597783cd9048c3f7df1b0
PHampy <philiphampy@autozone.com>
2017-01-24 09:33:45 -0600

    working UI for signature and submission page

--- app/main/controllers/review-submission-ctrl.js
+++ app/main/controllers/review-submission-ctrl.js
@@ -1,8 +1,86 @@
 'use strict';
 angular.module('myApp')
-.controller('ReviewSubmissionCtrl', function ($scope, $state) {
+.controller('ReviewSubmissionCtrl', function ($scope, $http, $state, DataService, $rootScope, $filter) {
+
+    $scope.max = 1000;
+    $scope.count = 0;
+
+    $scope.today = function() {
+      $scope.dt = new Date();
+    };
+
+    $scope.clear = function() {
+      $scope.dt = null;
+    };
+
+    $scope.inlineOptions = {
+      customClass: getDayClass,
+      minDate: new Date(),
+      showWeeks: true
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22),
+      minDate: new Date(),
+      startingDay: 1
+    };
+
+    $scope.toggleMin = function() {
+      $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+      $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+    };
+
+    $scope.toggleMin();
+
+    $scope.open1 = function() {
+      $scope.popup1.opened = true;
+    };
+
+    $scope.setDate = function(year, month, day) {
+      $scope.dt = new Date(year, month, day);
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd/MM/yyyy', 'MM/dd/yyyy', 'shortDate'];
+    $scope.format = $scope.formats[3];
+    $scope.altInputFormats = ['M!/d!/yyyy'];
+
+    $scope.popup1 = {
+      opened: false
+    };
+
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date();
+    afterTomorrow.setDate(tomorrow.getDate() + 1);
+    $scope.events = [
+      {
+        date: tomorrow,
+        status: 'full'
+      },
+      {
+        date: afterTomorrow,
+        status: 'partially'
+      }
+    ];
+
+    function getDayClass(data) {
+      var date = data.date,
+          mode = data.mode;
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0,0,0,0);
+        for (var i = 0; i < $scope.events.length; i++) {
+          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
+          }
+        }
+      }
+      return '';
+    }
 
 
+    $scope.saveInfo = function() {
 
-
+    };
 });
