3adde422d923196d87f876927ca987c9a01f8380
katherinetucker <ktucker1024@gmail.com>
2017-01-26 13:56:26 -0600

    #14: added status bar and filled in home page text

--- app/main/controllers/applicationStatus-ctrl.js
+++ app/main/controllers/applicationStatus-ctrl.js
@@ -1,41 +1,30 @@
 'use strict';
 angular.module('myApp')
-.controller('ApplicationStatusCtrl', function ($scope, $state, DataService, $timeout) {
-  //1. things first, let's see how you create a new application:
-  var requestContent = {foo: 'bar'};
-  DataService.createApplication(requestContent).then(function (result) {
-    if(result) {
-      var insertedApplication = result;
-      console.log('Originally inserted data:');
-      console.log(insertedApplication);
-      //The var "insertedApplication" above has the result of the insert, as does DataService.application
+.controller('ApplicationStatusCtrl', function ($scope, $state, $rootScope) {
 
-      //2. OK, how do you GET an application?
-      DataService.getApplicationById(insertedApplication._id).then(function (result) {
-        if(result) {
-          var gottenApplication = result;
-          console.log('GET data:');
-          console.log(gottenApplication);
-          //The var "gottenApplication" above has the result of the get, as does DataService.application
+  $scope.status = 'Approval Status';
+  $scope.isStep1Complete = false;
+  $scope.isStep2Complete = false;
+  $scope.isStep3Complete = false;
 
-          //3. OK, how do you UPDATE an application?
-          gottenApplication.request_content.updatedData = 'Here\'s a change';
-          DataService.updateApplication(gottenApplication).then(function(result) {
-            if(result) {
-              var updatedApplication = result;
-              console.log('Updated data:');
-              console.log(updatedApplication);
-              //For now, let's do it this way; this means we'll need a save button on each page, but converting it to be event-driven or watching the form adds a lot more complexity that we're not yet ready for.
-            } else {
-              //Todo: handle the error state
-            }
-          });
-        } else {
-          //Todo: handle the error state
-        }
-      });
-    } else {
-      //Todo: handle the error state
+  $scope.$watch('application', function (newValue) {
+    // newValue = undefined;
+    // $rootScope.application = undefined;
+    if (newValue) {
+      // $rootScope.application.status = 'Submitted';
+      if ($rootScope.application.status === 'Submitted') {
+        $scope.isStep1Complete = true;
+      } else if ($rootScope.application.status === 'Approver Pending') {
+        $scope.isStep1Complete = true;
+        $scope.isStep2Complete = true;
+      } else if (($rootScope.application.status === 'Returned') || ($rootScope.application.status === 'Approved') ||
+          ($rootScope.application.status === 'Denied') || ($rootScope.application.status === 'Completed')) {
+        $scope.isStep1Complete = true;
+        $scope.isStep2Complete = true;
+        $scope.isStep3Complete = true;
+        $scope.status = $rootScope.application.status;
+      }
     }
-  });
+  }, true);
+
 });
