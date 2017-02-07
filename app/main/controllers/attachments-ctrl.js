b7a993f94baf7395d0e25dce070cb18181f9d654
atcarnegie <Austin.Carnegie@autozone.com>
2017-02-07 17:01:52 -0600

    committing fix for retaining attachments if user leaves page and returns

--- app/main/controllers/attachments-ctrl.js
+++ app/main/controllers/attachments-ctrl.js
@@ -1,14 +1,26 @@
 'use strict';
 angular.module('myApp')
-.controller('AttachmentsCtrl', function ($scope, $state, $rootScope, DataService) {
+.controller('AttachmentsCtrl', function ($scope, $state, $rootScope, DataService, $stateParams) {
+
+    if(!$stateParams.appId) {
+      $state.go('applicationInformation', {});
+    } else if(!$rootScope.application || $rootScope.application._id != $stateParams.appId) {
+      DataService.getApplicationById($stateParams.appId).then(function (result) {
+        if(result) {
+          $rootScope.application = result;
+        } else {
+          //TODO: handle error
+        }
+      });
+    }
 
     $scope.saveLaterBtn = function() {
         $state.go('home');
     };
 
     $scope.name = '';
-    $scope.files = [];
-
+    $scope.files = $rootScope.application.documentation;
+    $scope.error = false;
     $scope.$watch('files', function(newValue) {
         console.log(newValue);
         if(newValue) {
@@ -16,7 +28,11 @@ angular.module('myApp')
                 DataService.updateAttachments($scope.files, $rootScope.application._id).then(function(result) {
                     if(result) {
                       var updatedAttachments = result;
+                      console.log('result');
+                      console.log($rootScope.application);
+                      $scope.error = false;
                     } else {
+                        console.log('error');
                       $scope.error = true;
                     }
                 });
