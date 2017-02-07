5eeca80e8870ee78de124f4519ce54d0c06e450d
PHampy <philiphampy@autozone.com>
2017-02-07 10:52:11 -0600

    #22 fixed bug fix issues

--- app/main/controllers/assistanceRequest-ctrl.js
+++ app/main/controllers/assistanceRequest-ctrl.js
@@ -13,13 +13,15 @@ angular.module('myApp')
     });
   }
 
+
+
 var data = $scope.user;
 $scope.submitForm = function(isValid) {
 
 $scope.submitted = true;
 
 if (( $scope.application.requestContent.assistanceRequested.fire || $scope.application.requestContent.assistanceRequested.other || $scope.application.requestContent.assistanceRequested.utilities||
-  $scope.application.requestContent.assistanceRequested.disaster || $scope.application.requestContent.assistanceRequested.funeral || $scope.user.options) == true){
+  $scope.application.requestContent.assistanceRequested.naturalDisaster || $scope.application.requestContent.assistanceRequested.funeral || $scope.user.options) == true){
   $state.go('other');
 }
 
