9e99731295449f6001f1762a4311ca34a2427e18
katherinetucker <ktucker1024@gmail.com>
2017-02-09 11:40:29 -0600

    #89: will only call create app once now, fixed so viewing app after submission won't create new one

--- app/main/controllers/applicationInformation-ctrl.js
+++ app/main/controllers/applicationInformation-ctrl.js
@@ -2,9 +2,11 @@
 angular.module('myApp')
 .controller('ApplicationInformationCtrl', function ($scope, $state, $rootScope, $stateParams, DataService, $location) {
   if(!$stateParams.appId) {
-    $rootScope.createApplication().then(function (result) {
-      $location.path('applicationInformation/' + $rootScope.application._id);
-    });
+    if ($stateParams.appId === null) {
+      $rootScope.createApplication().then(function (result) {
+        $location.path('applicationInformation/' + $rootScope.application._id);
+      });
+    }
   } else if(!$rootScope.application || $rootScope.application._id != $stateParams.appId) {
     DataService.getApplicationById($stateParams.appId).then(function (result) {
       if(result) {
@@ -17,7 +19,7 @@ angular.module('myApp')
 
   $scope.noWrapSlides = false;
   $scope.active = 0;
-  
+
   $scope.saveForLater = function() {
     $state.go('home');
   };
