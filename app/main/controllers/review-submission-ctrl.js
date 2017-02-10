63634c39bbdf7657d533dce12480b12e5116d52f
PHampy <philiphampy@autozone.com>
2017-02-10 10:34:07 -0600

    #22 fixed date for review page to save correctly, also added katherines draft work for the video

--- app/main/controllers/review-submission-ctrl.js
+++ app/main/controllers/review-submission-ctrl.js
@@ -1,16 +1,27 @@
 'use strict';
  angular.module('myApp')
 .controller('ReviewSubmissionCtrl', function ($scope, $state, DataService, $document, $uibModal, $rootScope, $filter, SignaturePad, $stateParams) {
+
+  var populateDate = function () {
+    if ($rootScope.application.requestContent.submitDetails.submitDate) {
+      $scope.dt = new Date($rootScope.application.requestContent.submitDetails.submitDate.$date);
+    }
+  };
+
+  $scope.appId = '';
   if(!$stateParams.appId) {
     $state.go('applicationInformation', {});
   } else if(!$rootScope.application || $rootScope.application._id != $stateParams.appId) {
     DataService.getApplicationById($stateParams.appId).then(function (result) {
       if(result) {
         $rootScope.application = result;
+        populateDate();
       } else {
         //TODO: handle error
       }
     });
+  } else {
+    populateDate();
   }
 
 var $ctrl = this;
@@ -35,6 +46,20 @@ var $ctrl = this;
       }
 
     };
+
+    $scope.dateChanged = function () {
+      $scope.showErrorMessage = false;
+      if ($rootScope.application) {
+        var date = new Date(document.getElementById('reviewDate').value);
+        $rootScope.application.requestContent.submitDetails = {
+          'submitDate' : { $date : date }
+        };
+      }
+    };
+
+
+
+
 
   $ctrl.animationsEnabled = true;
 
