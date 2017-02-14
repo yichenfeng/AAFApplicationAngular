6db2f3595c4f81f2b28ffe7def42adedc0aaa7e3
atcarnegie <Austin.Carnegie@autozone.com>
2017-02-14 10:10:53 -0600

    attachments page fix for duplicate uploads and refresh issues

--- app/main/controllers/attachments-ctrl.js
+++ app/main/controllers/attachments-ctrl.js
@@ -1,6 +1,8 @@
 'use strict';
 angular.module('myApp')
 .controller('AttachmentsCtrl', function ($scope, $state, $rootScope, DataService, $stateParams) {
+    // $scope.files = [];
+    $scope.error = false;
 
     if(!$stateParams.appId) {
       $state.go('applicationInformation', {});
@@ -12,24 +14,20 @@ angular.module('myApp')
           //TODO: handle error
         }
       });
-    }
+  }
 
     $scope.saveLaterBtn = function() {
         $state.go('home');
     };
 
-    $scope.name = '';
-    $scope.files = $rootScope.application.documentation;
-    $scope.error = false;
     $scope.$watch('files', function(newValue) {
-        console.log(newValue);
-        if(newValue) {
+        if(newValue && newValue.length > 0) {
             if($rootScope.application) {
-                DataService.updateAttachments($scope.files, $rootScope.application._id).then(function(result) {
+                DataService.updateAttachments(newValue[newValue.length - 1], $rootScope.application._id).then(function(result) {
                     if(result) {
-                      var updatedAttachments = result;
-                      console.log('result');
-                      console.log($rootScope.application);
+                        angular.forEach(result, function (doc) {
+                            $rootScope.application.documentation.push(doc);
+                        });
                       $scope.error = false;
                     } else {
                         console.log('error');
@@ -37,19 +35,24 @@ angular.module('myApp')
                     }
                 });
             }
+        } else {
+            //TODO:: handle nothing array (i.e. just deleted last item)
         }
     }, true);
 
     $scope.deleteAttachment = function(index) {
-        $scope.files.splice(index, 1);
+        $rootScope.application.documentation.splice(index, 1);
     };
 
-    $scope.addURL = function() {
-        $scope.files.push({fileName:$scope.urlLink, description: "", base64String: "" });
+    $scope.addURL = function(newValue) {
+        console.log('link');
+        $rootScope.application.documentation.push({fileName:$scope.urlLink, description: "", base64String: "" });
     };
 
     $scope.atMax = function() {
-        if ($scope.files.length > 20)
-            return true;
+        angular.element(document).ready(function () {
+            if ($rootScope.application.documentation.length > 20)
+                return true;
+        });
     };
 });
